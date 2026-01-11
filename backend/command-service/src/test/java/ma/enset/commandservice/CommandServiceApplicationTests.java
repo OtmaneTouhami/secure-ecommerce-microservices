d7f58d48e0b58eb1034373f4e8de2940b1d028da
Otmane Touhami <otmanetouhami01@gmail.com>
2026-01-11 13:53:05 +0100

    Add command service with Maven configuration, application properties

---
+++ backend/command-service/src/test/java/ma/enset/commandservice/CommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.commandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
